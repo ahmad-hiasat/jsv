258cb1b26543cac82de13b8843212d98eeb87266
ahmad hiasat <ahmadhiasat29@gmail.com>
2026-01-11 07:04:24 +0300

    Update users.ts

--- backend/database/users.ts
+++ backend/database/users.ts
@@ -11,7 +11,7 @@ const schema = new mongoose.Schema({
         type:String,
         required:true,
     },email:{
-        type:Stirng,
+        type:String,
         minlength : 5,
         unique:true
     }
